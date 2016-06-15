f4a09825b1374c509cfe180fcedd4bb387d200ff
Cristian Avina Lopez <cavinalopez@cpp.edu>
2016-06-14 22:07:30 -0700

    added functions to soundcloud music player feat. and fixed padding issue on picture page

--- js/soundcloudAPI.js
+++ js/soundcloudAPI.js
@@ -3,8 +3,14 @@
   widget.bind(SC.Widget.Events.READY, function() {
   console.log('Ready...');
   });
-     $('button').click(function() {
+     $('#play_pause_button').click(function() {
        widget.toggle();
      });
-   });
+     $('#next_button').click(function() {
+       widget.next();
+     });
+     $('#prev_button').click(function() {
+       widget.prev();
+     }); 
+  });
   
